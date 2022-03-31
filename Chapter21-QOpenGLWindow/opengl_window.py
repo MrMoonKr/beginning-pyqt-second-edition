9a2308345afec37b9c56cf362ebdd979ef42b7a6
Mr Moon <mrmoonkr@fixgames.co.kr>
2022-03-31 18:56:24 +0900

    requirements.txt 업데이트

--- Chapter21-QOpenGLWindow/opengl_window.py
+++ Chapter21-QOpenGLWindow/opengl_window.py
@@ -101,7 +101,7 @@ class MainWindow( QOpenGLWindow ):
     def paintGL(self) -> None:
         super().paintGL()
 
-        glClear( GL_COLOR_BUFFER_BIT )
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
 
         glUseProgram( self.program )
         glBindVertexArray( self.vao )
