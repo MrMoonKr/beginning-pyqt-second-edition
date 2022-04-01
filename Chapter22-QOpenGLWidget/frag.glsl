ba0791e5afded1a83a0051d96b0a9fa1eb6c98b4
Mr Moon <mrmoonkr@fixgames.co.kr>
2022-04-01 19:55:17 +0900

    로깅을 위젯에 붙여봄. 로깅 전용 위젯으로 만들어 사용하면 편할듯

---
+++ Chapter22-QOpenGLWidget/frag.glsl
@@ -0,0 +1,13 @@
+
+
+#version 330 core
+
+in vec3 fragmentColor;
+
+void main()
+{
+    gl_FragColor = vec4( fragmentColor, 1.0 );
+}
+
+
+
--- Chapter22-QOpenGLWidget/opengl_widget.py
+++ Chapter22-QOpenGLWidget/opengl_widget.py
@@ -1,9 +1,10 @@
 
 
+from asyncio.log import logger
 import sys
 
-from PyQt6.QtCore import ( Qt, QSize )
-from PyQt6.QtGui import ( QIcon, QSurfaceFormat )
+from PyQt6.QtCore import * #( Qt, QSize )
+from PyQt6.QtGui import * #( QIcon, QSurfaceFormat )
 from PyQt6.QtWidgets import * # ( QApplication )
 from PyQt6.QtOpenGL import * # ( QOpenGLWindow, QOpenGLVersionProfile )
 from PyQt6.QtOpenGLWidgets import * #( QOpenGLWidget )
@@ -13,97 +14,27 @@ from OpenGL.GL.shaders import *
 
 import numpy as np
 
+import logging
 
-vert_src = """
-#version 330 core
+# Uncomment below for terminal log messages
+logging.basicConfig( level=logging.DEBUG, 
+                     format=' %(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-layout ( location=0 ) in vec3 vertexPos;
-layout ( location=1 ) in vec3 vertexColor;
+class TextEditLogger( logging.Handler, QObject ):
+    appendPlainText = pyqtSignal( str )
 
-out  vec3 fragmentColor ;
-
-void main()
-{
-    gl_Position = vec4( vertexPos, 1.0 );
-    fragmentColor = vertexColor;
-}
-"""
-
-frag_src = """
-#version 330 core
-
-in vec3 fragmentColor;
-
-void main()
-{
-    gl_FragColor = vec4( fragmentColor, 1.0 );
-}
-"""
-
-
-class MainWindow( QOpenGLWindow ):
-    
-    def __init__( self ):
+    def __init__( self, target_widget ) -> None:
         super().__init__()
-        self.initUI()
+        QObject.__init__( self )
+        self.widget = target_widget
+        self.widget.setReadOnly( True )
+        self.appendPlainText.connect( self.widget.appendPlainText )
 
-    def initUI( self ):
-        self.setMinimumSize( QSize( 640, 480 ) )
-        self.setTitle( "OpenGL Window" )
-
-        self.show()
-
-
-    def initializeGL( self ) -> None:
-        self.profile = QOpenGLVersionProfile()
-        self.profile.setVersion( 3, 3 )
-        self.profile.setProfile( QSurfaceFormat.OpenGLContextProfile.CoreProfile )
-
-        print( f"OpenGL Version : { glGetString( GL_VERSION ) }" )
-
-        self.program = compileProgram( compileShader( vert_src, GL_VERTEX_SHADER ),
-                                       compileShader( frag_src, GL_FRAGMENT_SHADER ) )
-        
-        glUseProgram( self.program )
-
-        self.vertices = (
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0 ,
-             0.5, -0.5, 0.0, 0.0, 1.0, 0.0 ,
-             0.5,  0.5, 0.0, 0.0, 0.0, 1.0 
-        )
-        self.vertices = np.array( self.vertices, dtype=np.float32 )
-
-        self.vbo = glGenBuffers( 1 )
-        self.vao = glGenVertexArrays( 1 )
-        glBindVertexArray( self.vao )
-        glBindBuffer( GL_ARRAY_BUFFER, self.vao )
-
-        glBufferData( GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW )
-
-        glEnableVertexAttribArray( 0 )
-        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p( 0 ) )
-        
-        glEnableVertexAttribArray( 1 )
-        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p( 12 ) )
-
-
-
-        glClearColor( 0.3, 0.3, 0.3, 1.0 )
-
-        return super().initializeGL()
-
-    def resizeGL(self, w: int, h: int) -> None:
-        print( "resizeGL() 호출됨" )
-        return super().resizeGL(w, h)
-
-    def paintGL(self) -> None:
-        super().paintGL()
-
-        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
-
-        glUseProgram( self.program )
-        glBindVertexArray( self.vao )
-        glDrawArrays( GL_TRIANGLES, 0, 6 )
+    def emit( self, record ) -> None:
+        #msg = self.format( record )
+        #self.appendPlainText.emit( "record" )
+        msg = record.asctime + " : " + record.getMessage()
+        self.appendPlainText.emit( msg )
 
 class MainOpenGL( QOpenGLWidget ):
 
@@ -157,7 +88,8 @@ class MainOpenGL( QOpenGLWidget ):
         return super().initializeGL()
 
     def resizeGL( self, w: int, h: int ) -> None:
-        print( "resizeGL() 호출됨" )
+        #print( "resizeGL() 호출됨" )
+        logger.debug( "resizeGL() 호출됨" )
         self.width = w
         self.height = h
         glViewport( 0 , 0, w, h )
@@ -173,7 +105,7 @@ class MainOpenGL( QOpenGLWidget ):
         glBindVertexArray( self.vao )
         glDrawArrays( GL_TRIANGLES, 0, 6 )
 
-class MainFrame( QWidget ):
+class MainWindow( QWidget ):
 
     def __init__(self) -> None:
         super().__init__()
@@ -181,7 +113,7 @@ class MainFrame( QWidget ):
 
     def initUI( self ) -> None:
         self.setMinimumSize( 640, 480 )
-        self.setWindowTitle( "OpenGL Window" )
+        self.setWindowTitle( "OpenGL Widget" )
 
         self.tree_widget = QTreeWidget()
         #tree_widget.setColumnCount( 2 )
@@ -193,10 +125,17 @@ class MainFrame( QWidget ):
 
         self.opengl_widget = MainOpenGL()
 
-        self.splitter = QSplitter( Qt.Orientation.Horizontal )
-        self.splitter.addWidget( self.tree_widget )
-        #self.splitter.addWidget( self.list_widget )
+        self.textedit_widget = QPlainTextEdit()
+
+        self.log_widget = TextEditLogger( self.textedit_widget )
+        self.log_widget.setFormatter( logging.Formatter )
+        logging.getLogger().addHandler( self.log_widget )
+        logging.getLogger().setLevel( logging.DEBUG )
+
+
+        self.splitter = QSplitter( Qt.Orientation.Vertical )
         self.splitter.addWidget( self.opengl_widget )
+        self.splitter.addWidget( self.textedit_widget )
 
         main_h_box = QHBoxLayout()
         main_h_box.addWidget( self.splitter )
@@ -204,9 +143,13 @@ class MainFrame( QWidget ):
 
         self.show()
 
+        logging.debug('damn, a bug')
+        logging.info('something to remember')
+        logging.warning('that\'s not right')
+        logging.error('foobar')
+
 
 if __name__ == "__main__":
     app         = QApplication( sys.argv )
-    #main_window = MainWindow()
-    main_frame  = MainFrame()
+    main_window = MainWindow()
     sys.exit( app.exec() )
